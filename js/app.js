328aab5beecb2413a6338bae74709763a48714e4
Kevin de Roulet <kderoulet@yahoo.com>
2017-12-04 14:22:00 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(function() {    
+
+    // var sizeTheOverlays = function() {
+    //     $(".overlay").resize().each(function() {
+    //     var h = $(this).parent().outerHeight();
+    //     var w = $(this).parent().outerWidth();
+    //     $(this).css("height", h);
+    //     $(this).css("width", w);
+    //   });
+    // };
+
+    var sizeTheOverlays = function() {
+        $(".overlay").resize().each(function() {
+        let h = $(".portfolio-image").outerHeight();
+        let w = $(".portfolio-image").outerWidth();
+        $(this).css("height", h);
+        $(this).css("width", w);
+      });
+    };
+    
+    sizeTheOverlays();
+    
+    var width = $(window).width();
+    $(window).resize(function(){
+       if($(this).width() != width){
+          width = $(this).width();
+          sizeTheOverlays();
+       }
+    });
+})
