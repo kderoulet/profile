10e0a5f21195160200becfcf220d95783693c7dd
Kevin de Roulet <kderoulet@yahoo.com>
2017-12-06 19:04:38 -0800

    creates pages for portfolio pieces

--- js/app.js
+++ js/app.js
@@ -1,16 +1,54 @@
-$(function() {    
+$(function() {
+    
+    let portToWtpa = $("#portfolio-to-WTPA")
+    let wtpaToPort = $("#WTPA-to-portfolio")
+    let portToRyde = $("#portfolio-to-ryde")
+    let rydeToPort = $("#ryde-to-portfolio")
+    let portToDrag = $('#portfolio-to-dragonfighter')
+    let dragToPort = $('#dragonfighter-to-portfolio')
+    let portToSol = $('#portfolio-to-solitaire')
+    let solToPort = $('#solitaire-to-portfolio')
+    let portfolio = $("#portfolio")
+    let wtpa = $("#WTPA")
+    let ryde = $("#ryde")
+    let dragonfighter = $('#dragonfighter')
+    let solitaire = $('#solitaire')
 
-    $('#portfolio-to-WTPA').click(function() {
-        $('#portfolio').css("display", "block")
-        $('#WTPA').css("display", "none")
+    wtpaToPort.click(function() {
+        changeView(wtpa, portfolio)
     })
 
-    $('#WTPA-to-portfolio').click(function() {
-        $('#WTPA').css("display", "block")
-        $('#portfolio').css("display", "none")
+    portToWtpa.click(function() {
+        changeView(portfolio, wtpa)
     })
 
-    function displayPortfolio() {
-        
+    portToRyde.click(function() {
+        changeView(portfolio, ryde)
+    })
+
+    rydeToPort.click(function() {
+        changeView(ryde, portfolio)
+    })
+
+    portToDrag.click(function() {
+        changeView(portfolio, dragonfighter)
+    })
+
+    dragToPort.click(function() {
+        changeView(dragonfighter, portfolio)
+    })
+
+    portToSol.click(function() {
+        changeView(portfolio, solitaire)
+    })
+
+    solToPort.click(function() {
+        changeView(solitaire, portfolio)
+    })
+
+    function changeView(oldView, newView) {
+        oldView.css("display", "none")
+        newView.css("display", "block")
     }
+
 })
