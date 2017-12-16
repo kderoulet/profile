89b000b93a240687878f85d1ad7582fbdcd4afd2
Kevin de Roulet <kderoulet@yahoo.com>
2017-12-15 17:47:05 -0800

    adds react chess

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function() {
-    
+    let portToChess = $("#portfolio-to-reactchess")
+    let chessToPort = $("#reactchess-to-portfolio")
     let portToWtpa = $("#portfolio-to-WTPA")
     let wtpaToPort = $("#WTPA-to-portfolio")
     let portToRyde = $("#portfolio-to-ryde")
@@ -9,11 +10,19 @@ $(function() {
     let portToSol = $('#portfolio-to-solitaire')
     let solToPort = $('#solitaire-to-portfolio')
     let portfolio = $("#portfolio")
+    let chess = $("#reactchess")
     let wtpa = $("#WTPA")
     let ryde = $("#ryde")
     let dragonfighter = $('#dragonfighter')
     let solitaire = $('#solitaire')
 
+    chessToPort.click(function() {
+        changeView(chess, portfolio)
+    })
+
+    portToChess.click(function() {
+        changeView(portfolio, chess)
+    })
     wtpaToPort.click(function() {
         changeView(wtpa, portfolio)
     })
